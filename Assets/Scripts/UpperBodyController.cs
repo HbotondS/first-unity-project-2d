08df5fa73f9c061d8a76322e9fa80a9a06b48043
Botond Hegyi <hbotond97@gmail.com>
2021-04-14 21:20:28 +0300

    pickup handgun
    the player can pick up a handgun, add shoot, melee attack & reload animations

--- Assets/Scripts/UpperBodyController.cs
+++ Assets/Scripts/UpperBodyController.cs
@@ -7,5 +7,22 @@ public class UpperBodyController : MonoBehaviour
     void Update()
     {
         this.fistAnimator.SetBool("attack", Input.GetMouseButton(0));
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            this.fistAnimator.SetTrigger("isReloading");
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            this.fistAnimator.SetTrigger("meleeattack");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(other.gameObject);
+        if (other.gameObject.name.Equals("Pistol(Clone)"))
+        {
+            this.fistAnimator.SetBool("isGunEquipped", true);
+        }
     }
 }
