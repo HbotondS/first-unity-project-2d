9186f8a1c82d732364187d684a9deda1e2f5818e
Botond Hegyi <hbotond97@gmail.com>
2021-04-14 22:36:05 +0300

    shoot the bullet

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public Animator upperBodyAnimator;
+
+    public float bulletForce = 20f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && this.upperBodyAnimator.GetBool("isGunEquipped"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        var bullet = Instantiate(this.bulletPrefab, this.firePoint.position, this.firePoint.rotation);
+        // bullet.GetComponent<Rigidbody2D>().MoveRotation(-90f);
+        bullet.GetComponent<Rigidbody2D>().AddForce(this.firePoint.right * this.bulletForce, ForceMode2D.Impulse);
+    }
+}
